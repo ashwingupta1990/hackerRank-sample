7c41603c5390ef75151e78b86356061b1ff18983
ashwinkumargupta23 <ashwinkumargupta23@gmail.com>
2022-04-03 00:26:13 +0100

    first version

--- src/main/java/com/hackerrank/sample/controller/SampleController.java
+++ src/main/java/com/hackerrank/sample/controller/SampleController.java
@@ -1,8 +1,28 @@
 package com.hackerrank.sample.controller;
 
+import com.hackerrank.sample.dto.StringResponse;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SampleController {
-
+	 @ResponseStatus(HttpStatus.OK)
+	@RequestMapping(value = "/defaultHello", method = RequestMethod.GET )
+	public StringResponse defaultHelloWorld() {
+	return new StringResponse("Default Hello World!");
+	}
+	
+	@ResponseStatus(HttpStatus.OK)
+	@RequestMapping(value = "/customHello", method = RequestMethod.POST )
+	public StringResponse customHello(@RequestParam String message) {
+	return new StringResponse("Custom " + message);
+	
+	}
+	
+	
 }
